4e741ebb5752c224caba0f5365305cfaf27d533f
Adam Allan Thomas <you@example.com>
2023-03-25 17:57:52 -0600

    Here's the memo: I used memo

--- src/App.tsx
+++ src/App.tsx
@@ -1,76 +1,104 @@
-import React from "react";
+import React, { useMemo, useCallback } from "react";
 import Die from "./die";
 import { nanoid } from "nanoid";
 import Confetti from "react-confetti";
 
+interface DieProps {
+  value: number;
+  isHeld: boolean;
+  id: string;
+}
+
 export default function App() {
-    function allNewDice() {
-        let dice = [];
-        for (let i = 0; i < 10; i++) {
-          dice.push({
-            value: Math.floor(Math.random() * 6) + 1,
-            isHeld: false,
-            id: nanoid()
-          });
+  function allNewDice() {
+    let dice = [];
+    for (let i = 0; i < 10; i++) {
+      dice.push({
+        value: Math.floor(Math.random() * 6) + 1,
+        isHeld: false,
+        id: nanoid(),
+      });
+    }
+    return dice;
+  }
+  const [dice, setDice] = React.useState<DieProps[]>(allNewDice());
+  const [tenzies, setTenzies] = React.useState(false);
+
+  React.useEffect(() => {
+    const checkGameOver = () => {
+      if (dice.every((die) => die.value === dice[0].value && die.isHeld)) {
+        setTenzies(true);
+        console.log("You win!");
+      } else {
+        setTenzies(false);
+      }
+    };
+    checkGameOver();
+  }, [dice]);
+
+  const holdDice = useCallback((id: string) => {
+    setDice((prevDice) =>
+      prevDice.map((die) => {
+        if (die.id === id) {
+          return { ...die, isHeld: !die.isHeld };
+        } else {
+          return die;
         }
-        return dice;
-      }
-    const [dice, setDice] = React.useState(allNewDice());
-    const [tenzies, setTenzies] = React.useState(false);
-
-
-    React.useEffect(() => {
-        const checkGameOver = () => {
-          if (dice.every(die => die.value === dice[0].value && die.isHeld)) {
-            setTenzies(true);
-            console.log("You win!");
-          } else {
-            setTenzies(false);
-          }
-        };
-        checkGameOver();
-      }, [dice]);
-
-
-    function holdDice(id: string) {
-        setDice(prevDice => 
-          prevDice.map(die => {
-            if (die.id === id) {
-              return {...die, isHeld: !die.isHeld};
-            } else {
-              return die;
-            }
-          })
-        );
-      }
-      
-
-    function rollDice() {
-        setDice(prevDice => (
-          prevDice.map(die => {
-            if (die.isHeld) {
-              return die;
-            } else {
-              return {
-                ...die,
-                value: Math.floor(Math.random() * 6) + 1
-              };
-            }
-          })
-        ));
-      }
-      
-    return (
-        <main className="gamecontainer">
-            <h1>Tenzies</h1>
-            <p>Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
-            <div className="dice">
-                {dice.map((dieValue, index) => (
-                <Die key={dieValue.id} held={dieValue.isHeld} value={dieValue.value} holdDice={holdDice} id={dieValue.id} />
-                ))}
-            </div>
-            <button onClick={rollDice}>Roll</button>
-            {tenzies && <Confetti />}
-        </main>
+      })
     );
+  }, []);
+
+  function rollDice() {
+    setDice((prevDice) =>
+      prevDice.map((die) => {
+        if (die.isHeld) {
+          return die;
+        } else {
+          return {
+            ...die,
+            value: Math.floor(Math.random() * 6) + 1,
+          };
+        }
+      })
+    );
+  }
+
+  const dieLookup = useMemo(
+    () =>
+      dice.reduce(
+        (lookup, die) => ({
+          ...lookup,
+          [die.id]: (
+            <Die
+              key={die.id}
+              held={die.isHeld}
+              value={die.value}
+              holdDice={holdDice}
+              id={die.id}
+            />
+          ),
+        }),
+        {} as Record<string, React.ReactNode>
+      ),
+    [dice, holdDice]
+  );
+
+  const heldDice = useMemo(() => dice.filter((die) => die.isHeld), [dice]);
+  const notHeldDice = useMemo(() => dice.filter((die) => !die.isHeld), [dice]);
+
+  return (
+    <main className="gamecontainer">
+      <h1>Tenzies</h1>
+      <p>
+        Roll until all dice are the same. Click each die to freeze it at its
+        current value between rolls.
+      </p>
+      <div className="dice">
+        {heldDice.map((die) => dieLookup[die.id])}
+        {notHeldDice.map((die) => dieLookup[die.id])}
+      </div>
+      <button onClick={rollDice}>Roll</button>
+      {tenzies && <Confetti />}
+    </main>
+  );
 }
--- src/die.tsx
+++ src/die.tsx
@@ -1,17 +1,21 @@
-import React from 'react'
+import React from "react";
 
 interface DieProps {
-    value: number;
-    held: boolean;
-    id: string;
-    holdDice: (id: string) => void;
-  }
-
-
-export default function Die(props: DieProps) {
-    return (
-        <div onClick={() => props.holdDice(props.id)} className={props.held ? "dielock" : "die"}>
-            <h2>{props.value}</h2>
-        </div>
-    )
+  value: number;
+  held: boolean;
+  id: string;
+  holdDice: (id: string) => void;
 }
+
+const Die: React.FC<DieProps> = React.memo(function Die(props: DieProps) {
+  return (
+    <div
+      onClick={() => props.holdDice(props.id)}
+      className={props.held ? "dielock" : "die"}
+    >
+      <h2>{props.value}</h2>
+    </div>
+  );
+});
+
+export default Die;
